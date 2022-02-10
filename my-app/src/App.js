591c2ef2acfc3f34fcb5c75954db7171f96c0541
Gabriel Valle <you@example.comgabrielvallerodrigues@gmail.com>
2022-02-10 13:26:03 -0300

    Finalizada a aula de state lift

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
-import Condicional from './components/Condicional';
-import OutraLista from './components/OutraLista';
+import {useState} from 'react'
+import SeuNome from './components/SeuNome';
+import Saudacao from './components/Saudacao';
 
 
 
 function App() {
 
-  //reinderização de listas
-
-  const meusItens = ['React', 'veu', 'Angular']
+  const [nome,setNome] = useState()
    
   return (
     <div className="App">
-      <h1>Reinderização Condicional</h1>
-      <Condicional/>
-      <h1>Reinderização de listas</h1>
-      <OutraLista itens={meusItens} />
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>
     </div>
   );
 }
---
+++ my-app/src/components/SeuNome.js
@@ -0,0 +1,12 @@
+function SeuNome({setNome}){
+    return (
+        <>
+        <p>Digite o seu nome:</p>
+        <input type="text" 
+        placeholder="Qual é seu nome?"
+        onChange={(e) => setNome(e.target.value)}/>
+        </>
+    )
+}
+
+export default SeuNome
